3960b3be77555875d0df70f833cecf4373c3db55
Joel Borchardt <joelborchardt@Joels-MacBook-Pro.local>
2018-06-12 15:00:05 -0400

    added projects page

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'This is my projects page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Error handling this request'
